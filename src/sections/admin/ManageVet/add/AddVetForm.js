42cba73642278d7b010ec1148250c1ab25682f77
shoy777 <froilan1512@gmail.com>
2023-07-08 04:45:53 -0500

    Registro de Veterinaria

--- src/sections/admin/ManageVet/add/AddVetForm.js
+++ src/sections/admin/ManageVet/add/AddVetForm.js
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
-import { Button, Dialog, DialogTitle, DialogContent, DialogActions, TextField, Box } from '@mui/material';
-import swal from 'sweetalert';
+// Adecualo aqui la captura del value del autocomplete, ya que el valor del id se enviara al endpoint no el label
+import React, { useState, useEffect } from 'react';
+import { Button, Dialog, DialogTitle, DialogContent, DialogActions, TextField, Box, Autocomplete, FormControl, InputLabel, Select, MenuItem, Chip } from '@mui/material';
+import Swal from 'sweetalert2';
+import axios from 'axios';
 import Iconify from '../../../../components/iconify';
 
 const ImagePreview = ({ imageUrl }) => (
@@ -9,13 +11,143 @@ const ImagePreview = ({ imageUrl }) => (
   </div>
 );
 
+export const ERP = "https://lpwi1gf1y1.execute-api.us-east-1.amazonaws.com/dev/v1/";
+
 export default function AddVetForm() {
   const [open, setOpen] = useState(false);
+  const [speciesOptions, setSpeciesOptions] = useState([]);
+  const [selectedSpecies, setSelectedSpecies] = useState([]);
+  const [provinces, setProvinces] = useState([]);
+  const [districts, setDistricts] = useState([]);
+  
+  useEffect(() => {
+    fetch(`${ERP}species`)
+      .then((response) => response.json())
+      .then((data) => {
+        // Mapear los datos del endpoint para obtener las opciones del Autocomplete
+        const options = data.map((species) => ({
+          label: species.name,
+          value: species.idSpecie,
+        }));
+        setSpeciesOptions(options);
+      })
+      .catch((error) => {
+        console.error('Error al obtener los datos:', error);
+      });
+
+    const fetchProvinces = async () => {
+      try {
+        const responseProvinces = await axios.get(`${ERP}ubigeo/provinces`);
+        const provincesData = responseProvinces.data;
+
+        // Filtrar la lista de la provincia de Lima
+        const filteredProvinces = provincesData.filter(
+          (province) => province.idProvince === '1501'
+        );
+        
+        const responseDistricts = await axios.get(`${ERP}ubigeo/districts`);
+        const districtsData = responseDistricts.data;
+
+        // Filtrar la lista de los distritos de Lima
+        const filteredDistricts = districtsData.filter(
+          (district) => district.idProvince === '1501'
+        );
+
+        setProvinces(filteredProvinces);
+        setDistricts(filteredDistricts);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+
+    fetchProvinces();
+  }, []);
+
+  const fetchVetRegister = async (vetData) => {
+    try {
+      const responseUserRegister = await axios.post(`${ERP}vets`, JSON.stringify(vetData), {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      
+      if(responseUserRegister.status === 201){
+        Swal.fire({
+          title: 'Registrar Veterinaria',
+          text: 'Veterinaria Registrada. La veterinaria se ha guardado correctamente.',
+          icon: 'success',
+        });
+        // Limpiar el formulario
+        cleanForm();
+      } else {
+        Swal.fire({
+          title: 'Registrar Usuario',
+          text: 'No se ha podido guardar su información',
+          icon: 'warning',
+        });
+      }
+    } catch (error) {
+        // Manejar errores
+        const errorMessage = error.response.data;
+
+        if (error.response && error.response.status === 409) {
+          // El código de estado de la respuesta es 409 (Conflict)
+          // Maneja el caso de conflicto aquí
+          Swal.fire({
+            title: 'Registrar Usuario',
+            text: errorMessage.detail,
+            icon: 'error',
+          });
+        } else {
+          // Maneja otros errores de red u otras excepciones aquí
+          Swal.fire({
+            title: 'Registrar Usuario',
+            text: errorMessage.title,
+            icon: 'error',
+          });
+        }
+    }
+  };
+
+  const SweetAlertMessage = (title, type) => {
+    return Swal.fire({
+      title,
+      text: `¿Está seguro de ${type} la veterinaria?`,
+      icon: 'info',
+      allowOutsideClick: false,
+      showCancelButton: true,
+      confirmButtonText: type,
+      cancelButtonText: 'Cancelar',
+      showCloseButton: true,
+      showLoaderOnConfirm: true,
+      preConfirm: () => {
+        // navigate(link, { replace: true });
+      },
+      customClass: {
+        container: 'my-swal-container',
+      },
+    });
+  };
+
+  const sweetAlert = (title, message, icon) => {
+    Swal.fire({
+      title,
+      text: message,
+      icon,
+      customClass: {
+        container: 'my-swal-container',
+      },
+    });
+  };
+
   const [vetData, setVetData] = useState({
     vetName: '',
     vetAddress: '',
     vetPhone: '',
     vetDescription: '',
+    vetDistrict: null,
+    vetProvince: null,
     vetImage: null,
     submitClicked: false,
   });
@@ -25,15 +157,7 @@ export default function AddVetForm() {
   };
 
   const handleClose = () => {
-    setVetData({
-      vetName: '',
-      vetAddress: '',
-      vetPhone: '',
-      vetDescription: '',
-      vetImage: null,
-      submitClicked: false,
-    });
-    setOpen(false);
+    cleanForm();
   };
 
   const handleInputChange = (event) => {
@@ -62,30 +186,73 @@ export default function AddVetForm() {
     }));
 
     if (hasErrors) {
-      swal('Error', 'Por favor, completa todos los campos.', 'error');
+      sweetAlert('Error', 'Por favor, completa todos los campos.', 'error');
       return;
     }
 
     if (vetData.vetImage === null) {
-      swal('Error', 'Por favor, selecciona una imagen.', 'error');
+      sweetAlert('Error', 'Por favor, selecciona una imagen.', 'error');
       return;
     }
+    
+    if (selectedSpecies.length === 0) {
+      sweetAlert('Error', 'Por favor, selecciona al menos una especie.', 'error');
+      return;
+    }
+    
+    const vetServices = selectedSpecies.map((option) => ({
+      idVetService: option.value,
+    }));
 
-    // Si todos los campos están completos, muestra el mensaje de éxito
-    swal('Éxito', 'La veterinaria se ha guardado correctamente.', 'success');
+    const vetsData = {
+      "name": vetData.vetName,
+      "description": vetData.vetDescription,
+      "address": vetData.vetAddress,
+      "imageUrl": vetData.vetImage,
+      "phoneNumber": vetData.vetPhone,
+      "vetServices": vetServices,
+      "district": {
+        "idDistrict": vetData.vetDistrict
+      }
+    };
 
-    // Limpiar el formulario
+    SweetAlertMessage('Registrar Veterinaria', 'Registrar').then((result) => {
+      if (result.dismiss === Swal.DismissReason.close) {
+        // Código para manejar el cierre del diálogo
+      } else if (result.isConfirmed) {
+        // Código para manejar el clic en "Aceptar"
+        fetchVetRegister(vetsData);
+      } else if (result.isDenied) {
+        // Código para manejar el clic en "Cancelar"
+      }
+    });
+  };
+
+  const cleanForm = () => {
     setVetData({
       vetName: '',
       vetAddress: '',
       vetPhone: '',
       vetDescription: '',
+      vetDistrict: null,
+      vetProvince: null,
       vetImage: null,
       submitClicked: false,
     });
+    setSelectedSpecies([]);
     setOpen(false);
+  }
+  
+  const handleSpeciesChange = (event, values) => {
+    setSelectedSpecies(values);
+    setVetData((prevData) => ({
+      ...prevData,
+      vetServices: values.map((option) => ({
+        idVetService: option.value,
+      })),
+    }));
   };
-
+  
   return (
     <div>
       <Box justifyContent="space-between">
@@ -147,6 +314,63 @@ export default function AddVetForm() {
               error={vetData.vetDescription === '' && vetData.submitClicked}
               helperText={vetData.vetDescription === '' && vetData.submitClicked ? 'Este campo es requerido' : ''}
               sx={{ mb: 2 }}
+            />
+            <FormControl fullWidth required style={{ marginBottom: '20px' }}>
+              <InputLabel>Provincia</InputLabel>
+              <Select
+                name="vetProvince"
+                value={vetData.vetProvince}
+                onChange={handleInputChange}
+                error={vetData.vetProvince === null && vetData.submitClicked}
+                helperText={vetData.vetProvince === null && vetData.submitClicked ? 'Este campo es requerido' : ''} sx={{ mb: 2 }}
+              >
+                {provinces.map((province) => (
+                  <MenuItem key={province.idProvince} value={province.idProvince}>
+                    {province.name}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+            <FormControl fullWidth required style={{ marginBottom: '20px' }}>
+              <InputLabel>Distrito</InputLabel>
+              <Select
+                name="vetDistrict"
+                value={vetData.vetDistrict}
+                onChange={handleInputChange}
+                error={vetData.vetDistrict === null && vetData.submitClicked}
+                helperText={vetData.vetDistrict === null && vetData.submitClicked ? 'Este campo es requerido' : ''} sx={{ mb: 2 }}
+              >
+                {districts.map((district) => (
+                  <MenuItem key={district.idDistrict} value={district.idDistrict}>
+                    {district.name}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+            <Autocomplete
+              multiple
+              options={speciesOptions}
+              getOptionLabel={(option) => option.label}
+              value={selectedSpecies}
+              onChange={handleSpeciesChange}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  label="Selecciona servicio"
+                  error={selectedSpecies.length === 0 && vetData.submitClicked}
+                  helperText={selectedSpecies.length === 0 && vetData.submitClicked ? 'Este campo es requerido' : ''}
+                  limitTags={false}
+                  sx={{ mb: 2 }}
+                />
+              )}
+              renderTags={(value, getTagProps) =>
+                value.map((option, index) => (
+                  <Chip
+                    label={option.label}
+                    {...getTagProps({ index })}
+                  />
+                ))
+              } sx={{ mb: 2 }}
             />
             <input
               type="file"
